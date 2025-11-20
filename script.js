6f0fdd10eef8b0852c095b9ba48ffd85541dbc33
UMER~MEMON <unknownmemon690@gmail.com>
2025-11-20 16:30:43 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var name = prompt("Enter your name");
-var num1 = prompt("Hello " + name + " Enter first number");
-var num2 = prompt("Enter second number");
-
-var number1 = Number(num1);
-var number2 = Number(num2);
-alert("The sum is: " + (number1 + number2));
